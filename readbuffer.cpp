6492a8a295d79e4188e01978a8f48f6a5cbf1b23
ousttrue <ousttrue@gmail.com>
2024-01-31 02:26:55 +0900

    read utf8

--- readbuffer.cpp
+++ readbuffer.cpp
@@ -515,6 +515,14 @@ static int is_word_char(unsigned char *ch) {
   return 0;
 }
 
+static int is_combining_char(const char *ch) {
+  Lineprop ctype = get_mctype(ch);
+
+  if (ctype & PC_WCHAR2)
+    return 1;
+  return 0;
+}
+
 int is_boundary(unsigned char *ch1, unsigned char *ch2) {
   if (!*ch1 || !*ch2)
     return 1;
@@ -526,6 +534,9 @@ int is_boundary(unsigned char *ch1, unsigned char *ch2) {
     return 0;
 
   if (*ch2 != ' ' && is_beginning_char(ch1))
+    return 0;
+
+  if (is_combining_char((const char *)ch2))
     return 0;
 
   if (is_word_char(ch1) && is_word_char(ch2))
@@ -3790,7 +3801,15 @@ static void HTMLlineproc2body(Buffer *buf, Str *(*feed)(), int llimit) {
         PPUSH(PC_ASCII | effect | ex_efct(ex_effect), ' ');
         str++;
       } else if (*str != '<' && *str != '&') {
+        int len = get_mclen(str);
         PPUSH(mode | effect | ex_efct(ex_effect), *(str++));
+        if (--len) {
+          mode = (mode & ~PC_WCHAR1) | PC_WCHAR2;
+          while (len--) {
+            PSIZE;
+            PPUSH(mode | effect | ex_efct(ex_effect), *(str++));
+          }
+        }
       } else if (*str == '&') {
         /*
          * & escape processing
