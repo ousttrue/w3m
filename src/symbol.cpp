c35b4e19a1b096d8435da8202e6d1b6d917a7553
ousttrue <ousttrue@gmail.com>
2024-03-08 23:13:29 +0900

    push_symbol

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -9,17 +9,20 @@
 const char **get_symbol(void) { return alt_symbol; }
 
 void push_symbol(Str *str, char symbol, int width, int n) {
-  char buf[2];
-
-  auto p = alt_symbol[(unsigned char)symbol % N_SYMBOL];
-  int i = 0;
-  for (; i < 2 && *p; i++, p++)
-    buf[i] = (*p == ' ') ? (char)NBSP_CODE : *p;
-
-  Strcat(str, Sprintf("<_SYMBOL TYPE=%d>", symbol));
-  for (; n > 0; n--)
-    Strcat_charp_n(str, buf, i);
-  Strcat_charp(str, "</_SYMBOL>");
+  // char buf[2];
+  //
+  // auto p = alt_symbol[(unsigned char)symbol % N_SYMBOL];
+  // int i = 0;
+  // for (; i < 2 && *p; i++, p++)
+  //   buf[i] = (*p == ' ') ? (char)NBSP_CODE : *p;
+  //
+  // Strcat(str, Sprintf("<_SYMBOL TYPE=%d>", symbol));
+  // for (; n > 0; n--)
+  //   Strcat_charp_n(str, buf, i);
+  // Strcat_charp(str, "</_SYMBOL>");
+  for (int i = 0; i < n; ++i) {
+    Strcat_char(str, '*');
+  }
 }
 
 Str *conv_symbol(Line *l) {
