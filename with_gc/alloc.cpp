d94b3f18a2de742b391ae09b08edbab472815176
ousttrue <ousttrue@gmail.com>
2024-03-31 16:43:25 +0900

    remove gc

--- with_gc/alloc.cpp
+++
@@ -1,5 +0,0 @@
-#include "alloc.h"
-
-/* Define this as a separate function in case the free() has
- * an incompatible prototype. */
-void xfree(void *ptr) { free(ptr); }
--- with_gc/app.cpp
+++ with_gc/app.cpp
@@ -19,7 +19,6 @@
 #include "rc.h"
 #include "cookie.h"
 #include "history.h"
-#include "alloc.h"
 #include "http_response.h"
 #include <sys/stat.h>
 #include <iostream>
@@ -45,49 +44,6 @@ std::string CurrentCmdData;
 int prec_num = 0;
 bool on_target = true;
 #define PREC_LIMIT 10000
-
-static GC_warn_proc orig_GC_warn_proc = nullptr;
-#define GC_WARN_KEEP_MAX (20)
-
-void wrap_GC_warn_proc(char *msg, GC_word arg) {
-  if (App::instance().isRawMode()) {
-    /* *INDENT-OFF* */
-    static struct {
-      char *msg;
-      GC_word arg;
-    } msg_ring[GC_WARN_KEEP_MAX];
-    /* *INDENT-ON* */
-    static int i = 0;
-    static size_t n = 0;
-    static int lock = 0;
-    int j;
-
-    j = (i + n) % (sizeof(msg_ring) / sizeof(msg_ring[0]));
-    msg_ring[j].msg = msg;
-    msg_ring[j].arg = arg;
-
-    if (n < sizeof(msg_ring) / sizeof(msg_ring[0]))
-      ++n;
-    else
-      ++i;
-
-    if (!lock) {
-      lock = 1;
-
-      for (; n > 0; --n, ++i) {
-        i %= sizeof(msg_ring) / sizeof(msg_ring[0]);
-
-        printf(msg_ring[i].msg, (unsigned long)msg_ring[i].arg);
-        // sleep_till_anykey(1, 1);
-      }
-
-      lock = 0;
-    }
-  } else if (orig_GC_warn_proc)
-    orig_GC_warn_proc(msg, arg);
-  else
-    fprintf(stderr, msg, (unsigned long)arg);
-}
 
 static void *die_oom(size_t bytes) {
   fprintf(stderr, "Out of memory: %lu bytes unavailable!\n",
@@ -420,25 +376,6 @@ App::App() {
   _currentPid = getpid();
 
   ioutil::initialize();
-
-  if (!getenv("GC_LARGE_ALLOC_WARN_INTERVAL")) {
-    set_environ("GC_LARGE_ALLOC_WARN_INTERVAL", "30000");
-  }
-  GC_INIT();
-#if (GC_VERSION_MAJOR > 7) ||                                                  \
-    ((GC_VERSION_MAJOR == 7) && (GC_VERSION_MINOR >= 2))
-  GC_set_oom_fn(die_oom);
-#else
-  GC_oom_fn = die_oom;
-#endif
-
-#if (GC_VERSION_MAJOR > 7) ||                                                  \
-    ((GC_VERSION_MAJOR == 7) && (GC_VERSION_MINOR >= 2))
-  orig_GC_warn_proc = GC_get_warn_proc();
-  GC_set_warn_proc(wrap_GC_warn_proc);
-#else
-  orig_GC_warn_proc = GC_set_warn_proc(wrap_GC_warn_proc);
-#endif
 
   _tabs.push_back(TabBuffer::create());
 
