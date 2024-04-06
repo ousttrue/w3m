b86451ce9e326ef7d621f4c13865f01892a12a58
ousttrue <ousttrue@gmail.com>
2024-04-06 11:04:45 +0900

    WIP html tokenizer

--- tests/html_test.cpp
+++ tests/html_test.cpp
@@ -4,22 +4,51 @@
 #include <coroutine>
 
 // https://html.spec.whatwg.org/multipage/parsing.html
+// https://cpprefjp.github.io/lang/cpp20/coroutines.html
 
 enum HtmlTokenTypes {
+  Unknown,
   Character,
+  Reference,
 };
-
-enum HtmlParserStateType {};
 
 struct HtmlToken {
   HtmlTokenTypes type;
   std::string_view view;
+
+  bool operator==(const HtmlToken &rhs) const {
+    return type == rhs.type && view.size() == rhs.view.size() &&
+           view.begin() == rhs.view.begin();
+  }
+};
+
+inline std::ostream &operator<<(std::ostream &os, const HtmlToken &token) {
+  os << "[";
+  switch (token.type) {
+  case Unknown:
+    os << "Unknown";
+    break;
+  case Character:
+    os << "Character";
+    break;
+  case Reference:
+    os << "Reference";
+    break;
+  }
+  os << "] '" << token.view << "'";
+  return os;
+}
+
+enum HtmlParserStateType {
+  Data,
 };
 
 using HtmlParserResult =
     std::tuple<std::optional<HtmlParserStateType>, std::optional<HtmlToken>>;
 
-using HtmlParseStateFunc = std::function<HtmlParserResult(char8_t)>;
+using HtmlParseStateFunc =
+    std::function<std::tuple<std::string_view::iterator, HtmlParserResult>(
+        std::string_view::iterator)>;
 
 struct generator {
   struct promise_type;
@@ -45,7 +74,19 @@ public:
       return std::suspend_always{};
     }
   };
-  bool move_next() { return coro ? (coro.resume(), !coro.done()) : false; }
+  bool move_next() {
+    if (!coro) {
+      return false;
+    }
+    if (coro.done()) {
+      return false;
+    }
+    coro.resume();
+    if (coro.done()) {
+      return false;
+    }
+    return true;
+  }
   HtmlToken current_value() { return coro.promise().current_value; }
   generator(generator const &) = delete;
   generator(generator &&rhs) : coro(rhs.coro) { rhs.coro = nullptr; }
@@ -56,38 +97,67 @@ public:
 };
 
 struct DataState {
-  HtmlParserResult operator()(char8_t c) { return {}; }
+  std::tuple<std::string_view::iterator, HtmlParserResult>
+  operator()(std::string_view::iterator it) {
+    auto next = it + 1;
+    std::string_view view = {it, next};
+    return {next, {Data, HtmlToken{.type = Character, .view = view}}};
+  }
 };
 
-class HtmlTokenizer {
-  std::string_view _v;
-  std::string_view::iterator _current;
-  std::string_view::iterator _start;
-  HtmlParseStateFunc _state;
-
-public:
-  HtmlTokenizer(std::string_view v)
-      : _v(v), _current(_v.begin()), _start(_current) {
-    this->_state = DataState{};
-  }
-
-  generator next() {
-    while (_current != _v.end()) {
-      auto [next_state, value] = _state(*(_current++));
-      if (next_state) {
-        // _state = next_state;
-      }
-      if (value) {
-        co_yield *value;
-      }
+generator tokenize(std::string_view v) {
+  HtmlParseStateFunc state = DataState();
+  for (auto it = v.begin(); it != v.end();) {
+    auto [next, result] = state(it);
+    it = next;
+    auto [next_state, value] = result;
+    if (next_state) {
+      // _state = next_state;
+    }
+    if (value) {
+      co_yield *value;
     }
   }
-};
+}
 
 TEST(HtmlTest, tokenizer) {
-  HtmlTokenizer t(" ");
-  auto g = t.next();
-  g.move_next();
-  auto v = g.current_value().view;
-  EXPECT_EQ(" ", v);
+  {
+    auto src = " ";
+    auto g = tokenize(src);
+    EXPECT_TRUE(g.move_next());
+    auto token = g.current_value();
+    EXPECT_EQ(HtmlToken(Character, src), token);
+    EXPECT_FALSE(g.move_next());
+  }
+  {
+    auto src = "a b";
+    auto g = tokenize(src);
+    {
+      EXPECT_TRUE(g.move_next());
+      auto token = g.current_value();
+      EXPECT_EQ("a", token.view);
+    }
+    {
+      EXPECT_TRUE(g.move_next());
+      auto token = g.current_value();
+      EXPECT_EQ(" ", token.view);
+    }
+    {
+      EXPECT_TRUE(g.move_next());
+      auto token = g.current_value();
+      EXPECT_EQ("b", token.view);
+    }
+    EXPECT_FALSE(g.move_next());
+  }
+}
+
+TEST(HtmlTest, reference) {
+  {
+    auto src = "&amp;";
+    auto g = tokenize(src);
+    EXPECT_TRUE(g.move_next());
+    auto token = g.current_value();
+    EXPECT_EQ(HtmlToken(Reference, src), token);
+    EXPECT_FALSE(g.move_next());
+  }
 }
