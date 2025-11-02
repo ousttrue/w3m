19a1d52ea8b5999da5f03cb8118041e270d2f05b
ousttrue <ousttrue@gmail.com>
2025-11-02 13:14:58 +0900

    makeStrBuf

--- gcstr/Str.zig
+++ gcstr/Str.zig
@@ -1,18 +1,9 @@
 const std = @import("std");
 const c = @cImport({
     @cInclude("Str.h");
+    @cInclude("gc.h");
 });
 const GcAllocator = @import("GcAllocator.zig");
-
-fn sizeFromLen(len: c_int) usize {
-    if (len < c.INITIALStr_SIZE) {
-        return c.INITIALStr_SIZE - 1;
-    }
-    if (len >= c.STR_SIZE_MAX) {
-        return c.STR_SIZE_MAX - 1;
-    }
-    return @intCast(len);
-}
 
 export fn allocStr(_s: [*c]const u8, _len: c_int) [*c]u8 {
     const s = _s orelse return null;
@@ -21,31 +12,48 @@ export fn allocStr(_s: [*c]const u8, _len: c_int) [*c]u8 {
     if (len >= c.STR_SIZE_MAX)
         len = c.STR_SIZE_MAX - 1;
 
-    const allocator = GcAllocator.allocator();
-    const ptr = allocator.alloc(u8, len + 1) catch @panic("OOM");
-    std.mem.copyForwards(u8, ptr, s[0..len]);
+    const p: [*c]u8 = @ptrCast(c.GC_MALLOC_ATOMIC(len + 1));
+    const ptr = p orelse @panic("OOM");
+    std.mem.copyForwards(u8, ptr[0..len], s[0..len]);
     ptr[len] = 0;
-    return ptr.ptr;
+    return ptr;
 }
 
-/// size contains 0 terminate
-fn emptyStrFromSize(size: usize) c.Str {
+fn sizeFromLen(len: c_int) usize {
+    if (len < c.INITIALStr_SIZE) {
+        return c.INITIALStr_SIZE;
+    }
+    if (len >= c.STR_SIZE_MAX) {
+        return c.STR_SIZE_MAX;
+    }
+    return @intCast(len);
+}
+
+fn makeStrBuf(size: usize) []u8 {
     std.debug.assert(size > 0);
+    const p: [*c]u8 = @ptrCast(c.GC_MALLOC_ATOMIC(size));
+    const ptr: [*]u8 = p orelse @panic("OOM");
+    const buf = ptr[0..size];
+    buf[0] = 0;
+    return buf;
+}
+
+/// buf must 0 terminated
+fn makeStr(buf: []u8) c.Str {
     const allocator = GcAllocator.allocator();
     const x = allocator.create(c._Str) catch @panic("OOM");
     x.* = .{
-        .ptr = &(allocator.alloc(u8, size) catch @panic("OOM"))[0],
-        .area_size = size,
-        .length = 0,
+        .ptr = buf.ptr,
+        .area_size = buf.len,
+        .length = std.mem.indexOf(u8, buf, &.{0}) orelse @panic("must 0 terminated"),
     };
-    x.ptr[0] = 0;
     return x;
 }
 
 export fn Strnew() c.Str {
-    return emptyStrFromSize(c.INITIALStr_SIZE);
+    return makeStr(makeStrBuf(c.INITIALStr_SIZE));
 }
 
 export fn Strnew_size(len: c_int) c.Str {
-    return emptyStrFromSize(sizeFromLen(len));
+    return makeStr(makeStrBuf(sizeFromLen(len)));
 }
