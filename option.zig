839180ecaeea3108458e0a3c554d490b2242892e
ousttrue <ousttrue@gmail.com>
2026-01-10 21:19:24 +0900

    option.zig

--- option.zig
+++ option.zig
@@ -51,6 +51,110 @@ const Param = struct {
     getset: TypedGetSet,
     select: []SelectItem = &.{},
 
+    fn set(this: *@This(), value: [:0]const u8) void {
+        switch (this.getset) {
+            .P_INT => |getset| {
+                const ptr: *i32 = @ptrCast(@alignCast(getset.ptr));
+                if (this.input_type == c.PI_ONOFF) {
+                    ptr.* = if (c.str_to_bool(value.ptr, ptr.* != 0)) 1 else 0;
+                } else {
+                    if (std.fmt.parseInt(i32, value, 10)) |n| {
+                        ptr.* = n;
+                    } else |_| {}
+                }
+            },
+            .P_NZINT => |getset| {
+                const ptr: *i32 = @ptrCast(@alignCast(getset.ptr));
+                if (std.fmt.parseInt(i32, value, 10)) |n| {
+                    if (n > 0) {
+                        ptr.* = n;
+                    }
+                } else |_| {}
+            },
+            .P_SHORT => |getset| {
+                const ptr: *i16 = @ptrCast(@alignCast(getset.ptr));
+                if (this.input_type == c.PI_ONOFF) {
+                    ptr.* = if (c.str_to_bool(value.ptr, ptr.* != 0)) 1 else 0;
+                } else {
+                    if (std.fmt.parseInt(i16, value, 10)) |n| {
+                        ptr.* = n;
+                    } else |_| {}
+                }
+            },
+            .P_CHARINT => |getset| {
+                const ptr: *i8 = @ptrCast(@alignCast(getset.ptr));
+                if (this.input_type == c.PI_ONOFF) {
+                    ptr.* = if (c.str_to_bool(value.ptr, ptr.* != 0)) 1 else 0;
+                } else {
+                    if (std.fmt.parseInt(i8, value, 10)) |n| {
+                        ptr.* = n;
+                    } else |_| {}
+                }
+            },
+            .P_CHAR => |getset| {
+                const ptr: *u8 = @ptrCast(@alignCast(getset.ptr));
+                ptr.* = value[0];
+            },
+            .P_STRING => |getset| {
+                const ptr: *[*c]const u8 = @ptrCast(@alignCast(getset.ptr));
+                ptr.* = value.ptr;
+            },
+            .P_SSLPATH => |getset| {
+                const ptr: *[*c]const u8 = @ptrCast(@alignCast(getset.ptr));
+                if (value.len > 0) {
+                    ptr.* = c.rcFile(value.ptr);
+                } else {
+                    ptr.* = null;
+                }
+                c.getRuntime().*.ssl_path_modified = 1;
+            },
+            .P_COLOR => |getset| {
+                const ptr: *u8 = @ptrCast(@alignCast(getset.ptr));
+                ptr.* = str_to_color(value);
+            },
+            .P_CODE => |getset| {
+                const ptr: *u32 = @ptrCast(@alignCast(getset.ptr));
+                ptr.* = c.wc_guess_charset_short(value.ptr, ptr.*);
+            },
+            .P_PIXELS => |getset| {
+                const ptr: *f64 = @ptrCast(@alignCast(getset.ptr));
+                if (std.fmt.parseFloat(f64, value)) |ppc| {
+                    if (ppc >= c.MINIMUM_PIXEL_PER_CHAR and ppc <= c.MAXIMUM_PIXEL_PER_CHAR * 2) {
+                        ptr.* = ppc;
+                    }
+                } else |_| {}
+            },
+            .P_SCALE => |getset| {
+                const ptr: *f64 = @ptrCast(@alignCast(getset.ptr));
+                if (std.fmt.parseFloat(f64, value)) |ppc| {
+                    if (ppc >= 10 and ppc <= 1000) {
+                        ptr.* = ppc;
+                    }
+                } else |_| {}
+            },
+        }
+    }
+
+    // str to ansi color code
+    fn str_to_color(value: [:0]const u8) u8 {
+        return switch (std.ascii.toLower(value[0])) {
+            '0' => 0, // black
+            '1', 'r' => 1, // red
+            '2', 'g' => 2, // green
+            '3', 'y' => 3, // yellow
+            '4' => 4, // blue
+            '5', 'm' => 5, // magenta
+            '6', 'c' => 6, // cyan
+            '7', 'w' => 7, // white
+            '8', 't' => 8, // terminal
+            'b' => if (std.ascii.startsWithIgnoreCase(value, "blu"))
+                4 // blue
+            else
+                0, // black
+            else => 8, // terminal
+        };
+    }
+
     fn to_str(this: *@This()) [*c]const u8 {
         switch (this.getset) {
             .P_PIXELS, .P_SCALE => |getset| {
@@ -91,6 +195,11 @@ const Section = struct {
     params: std.ArrayList(Param) = .{},
 };
 
+const SectionRef = struct {
+    section: c.SettingsSections,
+    index: usize,
+};
+
 const Option = struct {
     allocator: std.mem.Allocator,
     sections: [c.SETTINGS_MAX]Section = .{
@@ -106,19 +215,22 @@ const Option = struct {
         .{ .name = "Charset Settings" },
     },
 
-    param_map: std.StringHashMap(*c.param_ptr) = undefined,
+    param_map_c: std.StringHashMap(*c.param_ptr) = undefined,
+    param_map_zig: std.StringHashMap(SectionRef) = undefined,
     display_charset_str: [*c]c.wc_ces_list,
 
     fn init(allocator: std.mem.Allocator) @This() {
         return .{
             .allocator = allocator,
-            .param_map = .init(allocator),
+            .param_map_c = .init(allocator),
+            .param_map_zig = .init(allocator),
             .display_charset_str = c.wc_get_ces_list(),
         };
     }
 
     fn deinit(this: *@This()) void {
-        this.param_map.deinit();
+        this.param_map_c.deinit();
+        this.param_map_zig.deinit();
     }
 
     fn register(this: *@This(), section: c.SettingsSections, p: *c.param_ptr) void {
@@ -197,8 +309,12 @@ const Option = struct {
                 }
             }
         }
+        this.param_map_zig.put(std.mem.span(p.name), .{
+            .section = section,
+            .index = this.sections[section].params.items.len,
+        }) catch {};
         this.sections[section].params.append(this.allocator, param) catch {};
-        this.param_map.put(std.mem.span(p.name), p) catch {};
+        this.param_map_c.put(std.mem.span(p.name), p) catch {};
     }
 
     fn panel(this: @This()) c.Str {
@@ -312,6 +428,14 @@ const Option = struct {
         c.Strcat_charp(src, "</table></form></body></html>");
         return src;
     }
+
+    fn get_param(this: *@This(), name: []const u8) ?*Param {
+        if (this.param_map_zig.get(name)) |section_index| {
+            return &this.sections[section_index.section].params.items[section_index.index];
+        } else {
+            return null;
+        }
+    }
 };
 
 var g_opts: Option = undefined;
@@ -357,14 +481,70 @@ export fn opt_register(section: c.SettingsSections, _p: [*c]c.param_ptr) void {
     g_opts.register(section, p);
 }
 
-export fn opt_get_param(name: [*c]const u8) [*c]c.param_ptr {
-    if (g_opts.param_map.get(std.mem.span(name))) |p| {
-        return p;
+export fn opt_load_panel() c.Str {
+    return g_opts.panel();
+}
+
+export fn opt_get_param_option(name: [*c]const u8) [*c]const u8 {
+    if (g_opts.param_map_zig.get(std.mem.span(name))) |section_index| {
+        const p = &g_opts.sections[section_index.section].params.items[section_index.index];
+        return p.to_str();
     } else {
         return null;
     }
 }
 
-export fn opt_load_panel() c.Str {
-    return g_opts.panel();
+export fn opt_set_param(_name: [*c]const u8, _value: [*c]const u8) bool {
+    const name: [*:0]const u8 = _name orelse {
+        return false;
+    };
+    const value: [*:0]const u8 = _value orelse {
+        return false;
+    };
+
+    const p = g_opts.get_param(std.mem.span(name)) orelse {
+        return false;
+    };
+    p.set(std.mem.span(value));
+
+    return true;
+}
+
+export fn opt_set_param_option(option: [*c]const u8) bool {
+    const tmp = c.Strnew();
+    var p = option;
+    // , *q;
+
+    while (p[0] != 0 and !c.IS_SPACE(p[0]) and p[0] != '=') : (p += 1) {
+        _ = c.Strcat_char(tmp, p[0]);
+    }
+    while (p[0] != 0 and c.IS_SPACE(p[0])) : (p += 1) {}
+    if (p[0] == '=') {
+        p += 1;
+        while (p[0] != 0 and c.IS_SPACE(p[0])) {
+            p += 1;
+        }
+    }
+    c.Strlower(tmp);
+    if (opt_set_param(tmp.*.ptr, p)) {
+        // goto option_assigned;
+    } else {
+        var q = tmp.*.ptr;
+        if (std.mem.startsWith(u8, std.mem.span(q), "no")) { // -o noxxx, -o no-xxx, -o no_xxx
+            q += 2;
+            if (q.* == '-' or q.* == '_')
+                q += 1;
+        } else if (tmp.*.ptr[0] == '-') { // -o -xxx
+            q += 1;
+        } else {
+            return false;
+        }
+
+        if (opt_set_param(q, "0")) {
+            // goto option_assigned;
+        } else {
+            return false;
+        }
+    }
+    return true;
 }
